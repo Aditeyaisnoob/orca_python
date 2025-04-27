3de21d3ee9c46feba0a5b4656c045ca7437971ce
Aditeyaisnoob <136694707+Aditeyaisnoob@users.noreply.github.com>
2025-04-27 14:01:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 from client import *
 from solana.publickey import PublicKey
 async def main():
-# here there is a logic to write the code for invoking the orcaClient Class
     orc =  OrcaClient("https://api.devnet.solana.com")
     print('reachead here')
     try:
