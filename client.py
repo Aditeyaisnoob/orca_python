af7f6673275c3fcd4c1d79b3192e009b48e3fd48
Aditeyaisnoob <136694707+Aditeyaisnoob@users.noreply.github.com>
2025-04-27 13:58:49 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -6,10 +6,7 @@ from solana.keypair import Keypair
 class OrcaClient:
     def __init__(self,rpc_url):
         self.client = AsyncClient(rpc_url)
-    async def find_pool(self,pool_pubkey: str):
-        pubkey = Pubkey.from_string(pool_pubkey)
-        resp = await self.client.get_account_info(pubkey)
-        return resp["result"]["value"]["data"][0]
+    
     async def send_transaction(self,payer:Keypair,tx:Transaction):
       try :
         tx_resp = await self.client.send_transaction(tx, payer, opts=TxOpts(skip_confirmation=False))
@@ -18,11 +15,18 @@ class OrcaClient:
         print(f'Something went wrong:{e}')
     async def parse_response(self, tx_sig: str):
         resp = await self.client.get_confirmed_transaction(tx_sig)
-        return resp
+        return resp.value
+    async def get_balance(self, public_key: PublicKey):
+        resp = await self.client.get_balance(public_key)
+        try:
+            balance = resp.value
+            return balance
+        except Exception as e:
+            print(f'{e}')
     async def close(self):
         await self.client.close()
     async def build_instruction(self,payer_pubkey: str,pool_pubkey: str,pool_authority_pubkey: str,pool_source_pubkey: str,pool_destination_pubkey: str,user_source_pubkey: str,user_destination_pubkey: str,token_program_id: str,amount_in: int,minimum_amount_out: int,program_id: str)-> Transaction:
-        keys = [AccountMeta(pubkey=Pubkey.from_string(pool_pubkey),is_signer=False, is_writable=False),AccountMeta(pubkey=Pubkey.from_string(pool_authority_pubkey), is_signer=False, is_writable=False),AccountMeta(pubkey=Pubkey.from_string(user_source_pubkey), is_signer=False, is_writable=True),AccountMeta(pubkey=Pubkey.from_string(pool_source_pubkey), is_signer=False, is_writable=True),AccountMeta(pubkey=Pubkey.from_string(pool_destination_pubkey), is_signer=False, is_writable=True),
+        keys = [AccountMeta(pubkey=Pubkey.from_string(pool_pubkey),is_signer=False, is_writable=False),AccountMeta(pubkey=Pubkey.from_string(pool_authority_pubkey), is_signer=False, is_writable=False),AccountMeta(pubkey=Pubkey.from_string(user_source_pubkey),is_signer=False,is_writable=True),AccountMeta(pubkey=Pubkey.from_string(pool_source_pubkey), is_signer=False, is_writable=True),AccountMeta(pubkey=Pubkey.from_string(pool_destination_pubkey), is_signer=False, is_writable=True),
 AccountMeta(pubkey=Pubkey.from_string(user_destination_pubkey),is_signer=False, is_writable=True),
 AccountMeta(pubkey=Pubkey.from_string(token_program_id),is_signer=False, is_writable=False),
 AccountMeta(pubkey=Pubkey.from_string(payer_pubkey),is_signer=True, is_writable=False)]
--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from solana.publickey import PublicKey
 async def main():
 # here there is a logic to write the code for invoking the orcaClient Class
     orc =  OrcaClient("https://api.devnet.solana.com")
-    # print('reachead here')
-    # try:
-    #     key = PublicKey("")
-    #     balance = await orc.get_balance(key)
-    #     print(balance)
-
-    # except Exception as e:
-    #     print('Something went wrong:{e}')
+    print('reachead here')
+    try:
+        key = PublicKey("")
+        balance = await orc.get_balance(key)
+        print(f'Balance: {balance} SOL')
+    except Exception as e:
+        print(f'Something went wrong:{e}')
     await orc.close()
 
 
